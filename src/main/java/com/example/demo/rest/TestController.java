567e35ac3d5a90f4b9a9a0fcdb77f576445288f2
Li-Yu, Yang <tony830802@gmail.com>
2023-06-24 11:34:43 +0800

    add counter

--- src/main/java/com/example/demo/rest/TestController.java
+++ src/main/java/com/example/demo/rest/TestController.java
@@ -1,13 +1,25 @@
 package com.example.demo.rest;
 
+import com.example.demo.DynamicThreadPoolExecutorMetrics;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class TestController {
 
+    private final DynamicThreadPoolExecutorMetrics metrics;
+
     @GetMapping("h")
     public boolean hello() {
+        metrics.getCounter("dept","it").increment();
+        return true;
+    }
+
+    @GetMapping("h2")
+    public boolean hello2() {
+        metrics.getCounter("dept","hr").increment();
         return true;
     }
 }
